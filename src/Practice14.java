b921dacaf9a7ee86e04992a72876f428d1830e5d
foagura <foagura_0416@yahoo.co.jp>
2024-09-11 14:43:25 +0900

    Initial commit

---
+++ src/Practice14.java
@@ -0,0 +1,11 @@
+public class Practice14 {
+    public static void main(String[] args){
+        /* do-while文 */
+        int a = 0;
+
+        do {
+            ++a;
+            System.out.println("aの値は" + a);
+        } while (a < 5);
+    }
+}
